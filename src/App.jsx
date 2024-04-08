0f2b7511159732d010b233ca10931c2285f2870c
michaeljdunn21 <michael.j.dunn@uconn.edu>
2024-04-08 13:54:20 -0400

    image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import insideLogo from './assets/InsideAirbnb.jfif';
 import './App.css';
 
 function App() {
