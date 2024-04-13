e4f6c003a7afcc88c55d5fd08be9c7cc4b950672
Zack Cruess <zackcruess@Zacks-MBP.lan>
2024-04-13 16:42:46 -0400

    google form link

--- src/App.jsx
+++ src/App.jsx
@@ -44,14 +44,14 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-        <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2FInsideAirbnb&psig=AOvVaw0nFH0VR8JbS_2vGWam3jlg&ust=1712953921495000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNilx-GAu4UDFQAAAAAdAAAAABAE" target="_blank">
+        <a href="https://forms.gle/EEQM9ZSU6oqjAaFk9" target="_blank">
           <img src="https://pbs.twimg.com/profile_images/575532099827986432/uiwyE4c1_400x400.png" className="logo insideairbnb" alt="InsideAirBnb logo" />
         </a>
         <a target="_blank">
           <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Connecticut_Huskies_logo.svg/640px-Connecticut_Huskies_logo.svg.png" className="logo uconn" alt="uconn logo" />
         </a>
       </div>
-      <h1>OPIM DATA VISUALIZATION</h1>
+      <h1>OPIM DATA VISUALIZATION 111</h1>
       <div className="card">
         <button onClick={getSheet}>Get Sheet</button>
         <table className="csv-table">
