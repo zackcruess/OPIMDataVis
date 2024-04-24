ba23e642fbe601e2beea53a183e5b46920967098
Zack Cruess <zackcruess@Zacks-MacBook-Pro.local>
2024-04-23 22:39:43 -0400

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -89,54 +89,55 @@ function App() {
 
   return (
     <>
-      <div style={{ backgroundColor: 'lightgray' }}></div>
-      <div style={{ backgroundColor: 'lightgray', padding: '10px', position: 'fixed', top: 0, left: 0, width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', zIndex: 999 }}>
-        <h1 style={{ color: 'darkblue', fontSize: '24px', margin: 0 }}>Inside AirBnb: Short-Term Rental Regulation Dashboard</h1>
-        <a href="https://forms.gle/UdAMsk1bL49Gegiv9" style={{ color: '#007bff', fontWeight: 'bold', textDecoration: 'none', marginLeft: '10px' }}>Submit a STR Regulation <span style={{ whiteSpace: 'nowrap' }}>Here</span></a>
-      </div>
-      <div style={{ paddingLeft: '20px', paddingTop: '60px', fontSize: '12pt', fontFamily: 'Calibri', maxWidth: '50vw' }}>
-        This is a website prototype done for our OPIM 3211 systems design and analysis project in assistance with InsideAirbnb. The goal of this project was to visualize data about short-term rental regulations. The data is constantly being updated, and if you would like to submit an STR Regulation, click the link in the righthand corner.
-      </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2FInsideAirbnb&psig=AOvVaw0nFH0VR8JbS_2vGWam3jlg&ust=1712953921495000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNilx-GAu4UDFQAAAAAdAAAAABAE" target="_blank">
-          <img src="https://pbs.twimg.com/profile_images/575532099827986432/uiwyE4c1_400x400.png" className="logo insideairbnb" alt="InsideAirBnb logo" />
-        </a>
-        <a target="_blank">
-          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Connecticut_Huskies_logo.svg/640px-Connecticut_Huskies_logo.svg.png" className="logo uconn" alt="uconn logo" />
-        </a>
-      </div>
-      <h1>OPIM DATA VISUALIZATION</h1>
-      <img src="https://i.imgur.com/GNXuZzp.png" alt="Bar Chart example" />
-      <canvas id="pieChart" width="200" height="200"></canvas>
-      <div className="card">
-        <button onClick={getSheet}>Get Sheet</button>
-        <table className="csv-table">
-          <thead>
-            <tr>
-              {csvArray[0].map((columnName, index) => (
-                <th key={index}>{columnName}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {csvArray.slice(1).map((row, rowIndex) => (
-              <tr key={rowIndex} className={rowIndex % 2 === 0 ? 'even-row' : 'odd-row'}>
-                {row.map((cell, cellIndex) => (
-                  <td key={cellIndex}>{cell}</td>
+      <div style={{ backgroundColor: 'white', minHeight: '200px' }}>
+        <div style={{ backgroundColor: 'white', padding: '10px', position: 'fixed', top: 0, left: 0, width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', zIndex: 999 }}>
+          <div>
+            <h1 style={{ color: 'darkblue', fontSize: '24px', margin: 0 }}>Inside AirBnb: Short-Term Rental Regulation Dashboard</h1>
+            <a href="https://forms.gle/UdAMsk1bL49Gegiv9" style={{ color: '#007bff', fontWeight: 'bold', textDecoration: 'none', marginLeft: '10px', fontFamily: 'Times New Roman', fontSize: '14pt' }}>Donate!</a>
+          </div>
+          <div>
+            <div style={{ backgroundColor: 'white', padding: '10px', fontFamily: 'Times New Roman', fontSize: '14pt' }}>
+              <span>Select State</span>
+              <select>
+                {states.map((state, index) => (
+                  <option key={index} value={state.value}>{state.name}</option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div style={{ paddingLeft: '20px', paddingTop: '60px', fontSize: '12pt', fontFamily: 'Calibri', maxWidth: '50vw' }}>
+          This is a website prototype done for our OPIM 3211 systems design and analysis project in assistance with InsideAirbnb. The goal of this project was to visualize data about short-term rental regulations. The data is constantly being updated, and if you would like to submit an STR Regulation, click the link in the righthand corner.
+        </div>
+        <h1>OPIM DATA VISUALIZATION</h1>
+        <img src="https://i.imgur.com/GNXuZzp.png" alt="Bar Chart example" />
+        <canvas id="pieChart" width="200" height="200"></canvas>
+        <div className="card">
+          <button onClick={getSheet}>Get Sheet</button>
+          <table className="csv-table">
+            <thead>
+              <tr>
+                {csvArray[0].map((columnName, index) => (
+                  <th key={index}>{columnName}</th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {csvArray.slice(1).map((row, rowIndex) => (
+                <tr key={rowIndex} className={rowIndex % 2 === 0 ? 'even-row' : 'odd-row'}>
+                  {row.map((cell, cellIndex) => (
+                    <td key={cellIndex}>{cell}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
 }
+
+
 
 export default App;
